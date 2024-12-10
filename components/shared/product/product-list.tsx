5bc7e776cf6ac57ea7511af583268b6f27dfc299
Yiseul park <elane94@naver.com>
2024-12-10 10:48:09 +0900

    08. deploy on vercel

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -3,13 +3,13 @@ import React from 'react'
 import ProductCard from './product-card'
 import { Product } from '@/types'
 
-const ProductList = ({ data }: { data: Product[] }) => {
+const ProductList = ({ data }: { title: string; data: Product[] }) => {
   return (
     <>
       {data.length > 0 ? (
         <div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-            {data.map((product) => (
+            {data.map((product: Product) => (
               <ProductCard key={product.slug} product={product} />
             ))}
           </div>
